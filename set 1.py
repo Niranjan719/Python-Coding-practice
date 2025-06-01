620bad6a5e6b1eb718c6c0f954b432adad71735d
Niranjan <niranjansaikoppisetti@gmail.com>
2025-06-01 19:18:14 +0530

    chanegs

--- set 1.py
+++ set 1.py
@@ -2,24 +2,24 @@
 # Write a Python function that returns the k most frequent elements in descending order of frequency. 
 # nums - [1,2,1, 2, 1, 3, 4, 5, 4, 4],  k=2
 
-# def most_frequest_nums(nums,k):
-#     empty_dict = {}
-#     for num in nums:
-#         empty_dict[num] = empty_dict.get(num, 0) + 1
-#     work_list = list(empty_dict.items())
-#     sorted_list = sorted(work_list, key=lambda x: x[1], reverse = True)
-#     answer_list = []
-#     for item in sorted_list[:k]:
-#         answer_list.append(item[0])
-#     return answer_list
+def most_frequest_nums(nums,k):
+    empty_dict = {}
+    for num in nums:
+        empty_dict[num] = empty_dict.get(num, 0) + 1
+    work_list = list(empty_dict.items())
+    sorted_list = sorted(work_list, key=lambda x: x[1], reverse = True)
+    answer_list = []
+    for item in sorted_list[:k]:
+        answer_list.append(item[0])
+    return answer_list
     
-# list1 = [1,2,1, 2, 1, 3, 4, 5, 4, 4]
-# k = 2
+list1 = [1,2,1, 2, 1, 3, 4, 5, 4, 4]
+k = 2
 
-# ans = most_frequest_nums(list1,k)
-# print(ans)
+ans = most_frequest_nums(list1,k)
+print(ans)
 
-# Given a list of integers and a target sum, 
+#Q2. Given a list of integers and a target sum, 
 # return all unique pairs of elements that add up to the target.
 # nums = [2, 4, 3, 5, 7, 8, 1], target = 9 
 
